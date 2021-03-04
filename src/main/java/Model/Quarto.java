c80f15b23bd108970b9d7684987ea4540eb76e87
Ruben Brouwers <ruben.brouwers@student.kdg.be>
2021-03-04 08:23:19 +0100

    JavaFX model class structure

--- src/main/java/Model/Quarto.java
+++ src/main/java/Model/Quarto.java
@@ -1,5 +1,15 @@
 package main.java.Model;
 
 public class Quarto {
-	
+    // private attributes
+
+    public Quarto() {
+// Constructor
+
+    }
+
+// methods with business logic
+
+// needed getters and setters
+        
 }
