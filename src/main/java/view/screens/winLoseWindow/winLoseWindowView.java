24d92abbc454510fdd80b4233a58b550f66b21e9
Ruben Brouwers <ruben.brouwers@student.kdg.be>
2021-03-17 00:15:14 +0100

    winLoseWindow added

---
+++ src/main/java/view/screens/winLoseWindow/winLoseWindowView.java
@@ -0,0 +1,27 @@
+package main.java.view.screens.winLoseWindow;
+
+import javafx.scene.layout.GridPane;
+
+public class winLoseWindowView extends GridPane {
+    // private Node attributes (controls)
+
+    public winLoseWindowView() {
+        initialiseNodes();
+        layoutNodes();
+    }
+
+    private void initialiseNodes() {
+        // create and configure controls
+        // button = new Button("...")
+        // label = new Label("...")
+    }
+
+    private void layoutNodes() {
+        // add/set … methods
+        // Insets, padding, alignment, …
+    }
+
+    // package-private Getters
+    // for controls used by Presenter
+}
+
