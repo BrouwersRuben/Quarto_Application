43de691e96ab24e61e36db7f19f78645046ea5ea
rodzhers <rodzers.usackis@student.kdg.be>
2021-05-01 17:15:50 +0200

    temporary miscallaneous

---
+++ src/main/java/model/board/Pieces.java
@@ -0,0 +1,8 @@
+package main.java.model.board;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Pieces {
+    private ArrayList<Integer> remainingPieces = new ArrayList<Integer>(Collections.nCopies(16, 0));
+}
