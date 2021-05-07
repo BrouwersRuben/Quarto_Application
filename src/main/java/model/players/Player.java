48494fd5d8b977c18b77900ed59b8ded95c79475
Ruben Brouwers <ruben.brouwers@student.kdg.be>
2021-05-07 15:34:36 +0200

    javadoc stuff

--- src/main/java/model/players/Player.java
+++ src/main/java/model/players/Player.java
@@ -1,5 +1,12 @@
 package main.java.model.players;
 
+/**
+ * This is the main class of the players of the game, these classes are made to store data on these players, like:
+ * Is it a computer, the name, the time played...
+ * @version 1.0
+ * @author Rodžers Ušackis
+ * @author Ruben Brouwers
+ */
 public class Player {
     private boolean hasQuarto = false;
 
