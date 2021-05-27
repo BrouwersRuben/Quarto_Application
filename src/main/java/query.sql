93f41beabaf70bfaa08f15f1cc380eae6aaf8142
rodzhers <rodzers.usackis@student.kdg.be>
2021-05-27 23:03:40 +0200

    query fits your gameWon YES NO instead of 1 0 preferences

--- src/main/java/query.sql
+++ src/main/java/query.sql
@@ -1,5 +1,4 @@
-SELECT id, username, date_started, score, turns, time_played, game_difficulty, has_quarto, TO_CHAR(time_played/turns, '99.99') AS "Average Move Duration"
+SELECT id, username, date_started, score, turns, time_played, game_difficulty, (CASE WHEN has_quarto = 1 THEN 'YES' ELSE 'NO' END) has_quarto, TO_CHAR(time_played/turns, '99.99') AS "Average Move Duration"
 FROM game_data;
 
 
-SELECT ROUND(SUM(score)/COUNT(*), 2) from game_data WHERE score!=0;
